3b7a11e605a1508d55917f7d3556e544214f2f08
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-10-30 14:28:43 -0300

    docs: agregue swagger v1

--- Program.cs
+++ Program.cs
@@ -5,10 +5,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Pomelo.EntityFrameworkCore.MySql;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// --- Configuración de la base de datos ---
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<inmobiliaria.Models.DataContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
@@ -16,10 +17,9 @@ builder.Services.AddDbContext<inmobiliaria.Models.DataContext>(options =>
 // --- Controladores y vistas ---
 builder.Services.AddControllersWithViews();
 
-// --- Autenticación por Cookies 
+// --- Autenticación por Cookies (Tu configuración)
 builder.Services.AddAuthentication(options =>
 {
-    // Configuración combinada de esquemas
     options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
     options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -31,7 +31,7 @@ builder.Services.AddAuthentication(options =>
     options.LogoutPath = "/Usuario/Logout";
     options.AccessDeniedPath = "/Home";
 })
-// Autenticación por JWT
+// Autenticación por JWT (Tu configuración)
 .AddJwtBearer(options =>
 {
     var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]);
@@ -64,10 +64,64 @@ builder.Services.AddScoped<inmobiliaria.Models.RepositorioAuditoria>();
 builder.Services.AddScoped<AuditoriaHelper>();
 builder.Services.AddScoped<inmobiliaria.lib.HashPasswordService>();
 
+
+
+// 1. Agrega el explorador de Endpoints (necesario para Swagger)
+builder.Services.AddEndpointsApiExplorer();
+
+// 2. Agrega el generador de Swagger (ANTES de builder.Build())
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "API Inmobiliaria",
+        Version = "v1"
+    });
+
+    // 3. Configuración para que Swagger entienda la autorización JWT
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Ingresa 'Bearer' [espacio] y luego tu token JWT. \r\n\r\nEjemplo: 'Bearer 12345abcdef'"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+
+
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
+
+if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Inmobiliaria v1");
+    });
+}
+else
+{
+
     app.UseExceptionHandler("/Home/Error");
     app.UseStatusCodePagesWithReExecute("/Home/StatusCode", "?code={0}");
     app.UseHsts();
